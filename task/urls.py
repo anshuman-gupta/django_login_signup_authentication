a18f351cc2602899bf799914aa85af2851a37658
mr_gupta_ji <anshumangupta5050@gmail.com>
2020-04-16 14:52:18 +0530

    commit

--- task/urls.py
+++ task/urls.py
@@ -19,6 +19,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/',include('account.urls')),
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path("register",views.register,name="register"),
+    path("login",views.login,name="login"),
+    path("logout",views.logout,name="logout")
 ]
